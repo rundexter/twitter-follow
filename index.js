71585092f029027a8ebd1c492f404233c8dbe856
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-18 12:46:29 +0200

    Add Twitter Follow module

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var Twitter = require('twitter');
+var optionPrefix = 'twitter_';
+var requiredEnv = ['twitter_consumer_key', 'twitter_consumer_secret', 'twitter_access_token', 'twitter_access_token_secret'];
+
 module.exports = {
+    follow: function (authOptions, params, callback) {
+        var client = new Twitter(authOptions);
+        
+        // Allows the authenticating users to follow the user specified in the ID parameter.
+        client.get('friendships/create', params, callback);
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +16,30 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        // twitter auth property
+        var authOptions = {};
+
+        requiredEnv.forEach(function (envVar) {
+
+            if(dexter.environment(envVar)) {
+                // get auth property
+                var prop = envVar.substr(optionPrefix.length -1, -1);
+
+                authOptions[prop] = dexter.environment(envVar);
+            } else {
+
+                this.fail('A ' + envKey + ' environment variable is required for this module');
+            }
+        });
+
+        this.follow(authOptions, step.inputs(), function (error, tweets) {
+
+            if (error) {
+                // if error - send message
+                this.fail(error);
+            }
+            // return tweets
+            this.comcomplete(tweets);
+        }.bind(this));
     }
 };

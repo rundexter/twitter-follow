79b224955e8a41322922b8b8a5fe5d22d23054ae
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-25 20:00:46 +0200

    fix

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
 var Twit = require('twit');
 var _ = require('lodash');
 
-var mapAuthOptionsEnv = {
-    'twitter_consumer_key': 'consumer_key',
-    'twitter_consumer_secret': 'consumer_secret',
-    'twitter_access_token': 'access_token',
-    'twitter_access_token_secret': 'access_token_secret'
-};
-
-
 module.exports = {
-    follow: function (authOptions, params, callback) {
-        var twitter = new Twit(authOptions);
-
-        // Follow befriended
-        twitter.post('friendships/create', params, callback);
-    },
-
-    /**
-     * Get Auth options from Environment.
-     *
-     * @param dexter
-     * @returns {{}}
-     */
-    authOptions: function (dexter) {
-        // twitter auth property
-        var authOptions = {};
-
-        _.map(mapAuthOptionsEnv, function (authOpt, twitterOpt) {
-            if(dexter.environment(twitterOpt)) {
-                // get auth property
-                authOptions[authOpt] = dexter.environment(twitterOpt);
-            } else {
-                // catch no-arguments message
-                this.fail('A ' + twitterOpt + ' environment variable is required for this module');
-            }
-        }, this);
-
-        return authOptions;
-    },
-
     /**
      * Allows the authenticating users to follow the user specified in the ID parameter.
      *
@@ -47,8 +9,15 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
+        var credentials = dexter.provider('twitter').credentials(),
+            twitter = new Twit({
+                access_token: credentials.access_token,
+                access_token_secret: credentials.access_token_secret,
+                consumer_key: credentials.consumer_key,
+                consumer_secret: credentials.consumer_secret
+            });
 
-        this.follow(this.authOptions(dexter), step.inputs(), function (error, befriendedInfo) {
+        twitter.post('friendships/create', step.inputs(), function (error, befriendedInfo) {
             if (error) {
                 // if error - send message
                 this.fail(error);
